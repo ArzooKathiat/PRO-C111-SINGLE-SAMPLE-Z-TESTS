4182db506201ac20cbebc66f4a7b4a5256760506
Ash Kathiat <69519775+ArzooKathiat@users.noreply.github.com>
2021-06-05 09:07:48 +0530

    Update z-score.py

--- z-score.py
+++ z-score.py
@@ -5,14 +5,10 @@ import random
 import pandas as pd
 import csv
 
-#Change the School data here
 df = pd.read_csv("School2.csv")
 data = df["Math_score"].tolist()
 
 
-##  code to find the mean of 100 data points 1000 times 
-#function to get the mean of the given data samples
-# pass the number of data points you want  as counter
 def random_set_of_mean(counter):
     dataset = []
     for i in range(0, counter):
@@ -23,14 +19,12 @@ def random_set_of_mean(counter):
     return mean
 
 
-# Function to get the mean of 100 data sets
+
 mean_list = []
 for i in range(0,1000):
     set_of_means= random_set_of_mean(100)
     mean_list.append(set_of_means)
 
-
-## calculating mean and standard_deviation of the sampling distribution.
 std_deviation = statistics.stdev(mean_list)
 mean = statistics.mean(mean_list)
 print("mean of sampling distribution:- ",mean)
@@ -38,7 +32,6 @@ print("Standard deviation of sampling distribution:- ", std_deviation)
 
 
 
-## findig the standard deviation starting and ending values
 first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
 second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
 third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
@@ -46,10 +39,6 @@ third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mea
 # print("std2",second_std_deviation_start, second_std_deviation_end)
 # print("std3",third_std_deviation_start,third_std_deviation_end)
 
-
-
-
-# # finding the mean of THE STUDENTS WHO GAVE EXTRA TIME TO MATH LAB and plotting on graph
 df = pd.read_csv("School_1_Sample.csv")
 data = df["Math_score"].tolist()
 mean_of_sample1 = statistics.mean(data)
@@ -62,10 +51,6 @@ fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end],
 fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
 fig.show()
 
-
-
-
-# #finding the mean of the STUDENTS WHO USED MATH PRACTISE APP and plotting it on the plot.
 df = pd.read_csv("School_2_Sample.csv")
 data = df["Math_score"].tolist()
 mean_of_sample2 = statistics.mean(data)
@@ -79,7 +64,6 @@ fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y
 fig.show()
 
 
-# finding the mean of the STUDENTS WHO WERE ENFORCED WITH REGISTERS and plotting it on the plot.
 df = pd.read_csv("School_3_Sample.csv")
 data = df["Math_score"].tolist()
 mean_of_sample3 = statistics.mean(data)
@@ -92,6 +76,5 @@ fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y
 fig.show()
 
 
-#finding the z score using the formula
 z_score = (mean - mean_of_sample2)/std_deviation
 print("The z score is = ",z_score)
